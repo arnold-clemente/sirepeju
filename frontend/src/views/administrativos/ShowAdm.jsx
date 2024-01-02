937a9337b83c841b24601e7c2240435ef35728c6
arnold-clemente <h.c.harnaldo@gmail.com>
2024-01-02 15:50:43 -0400

    showAdmin

--- frontend/src/views/administrativos/ShowAdm.jsx
+++ frontend/src/views/administrativos/ShowAdm.jsx
@@ -9,19 +9,13 @@ const ShowAdm = ({ modal, close, registro }) => {
     return (
         <>
             <ModalDiv isOpen={modal} closeModal={close} title={'LISTA DEL PERSONAL ADMINISTRATIVO UPJ - VA'}>
-
-
                 <div className='container-fluid d-flex justify-content-end'>
                     <button onClick={openImprimir} className='button_print_show'>
                         <i className="fa-solid fa-print"></i>
                         <span>Imprimir Reporte</span>
                     </button>
                 </div>
-
-
                 <RepAdm registro={registro} modal={imprimir} close={closeImprimir} />
-
-<<<<<<< Updated upstream
                 <div className="modal-dialog">
                     <div className="card m-2">
                         <div className="card-header">
@@ -63,23 +57,10 @@ const ShowAdm = ({ modal, close, registro }) => {
                             <div className="col-md-8 my-1 text-uppercase">
                             {registro.usuario}
                             </div>
-
                            </div>
                         </div>
-                    </div> 
-=======
-                <div className="modal-dialog modal-lg">
-                    <h2 className="fs-6"><b>Nombre Completo:</b> &nbsp;&nbsp;{registro.nombres + ' ' + registro.paterno + ' ' + registro.materno} </h2>&nbsp;&nbsp;<b>CI:</b> {registro.ci + ' ' + registro.ext_ci}<hr />
-                    <h2 className="fs-6"><b>Cargo:</b> &nbsp;{registro.cargo}</h2> <hr />
-                    <div
-                    ><h2 className="fs-6"><b>Correo Electronico:</b> &nbsp;{registro.email}</h2><hr />
-
-                        <h2 className="fs-6"><b>Rol:</b> &nbsp;{registro.usuario}</h2> <hr />
-                    </div>
-
->>>>>>> Stashed changes
+                    </div>               
                 </div>
-
                 <div className='d-flex'>
                     <button className="btn btn-secondary" onClick={close}>cerrar</button>
                 </div>
